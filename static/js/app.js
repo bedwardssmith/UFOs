bbae9bf6c0f6ceb410430ba39fd8a184228a854e
Bev Edwards-Smith <bedwardssmith@yahoo.ca>
2021-03-06 15:42:32 -0500

    added all functions to js file

--- static/js/app.js
+++ static/js/app.js
@@ -14,7 +14,7 @@ function buildTable(data) {
     // next loop through each object in the data
     // and append a row and cells for each value in the row
     data.forEach((dataRow) => {
-        
+
         // Append a row the the table body
         let row = tbody.append("tr");
 
@@ -27,3 +27,29 @@ function buildTable(data) {
         );
     });
 }
+
+// function to filter the table by date
+function handleClick() {
+    
+    // create variables to hold date data both filtered and unfiltered
+    let date = d3/select("#datetime").property("value");
+
+    // set a default filter and save it to a new variable
+    let filteredData = tableData;
+
+    // check to see if a date was entered and filter the 
+    // data using that date.
+    if (date) {
+        
+        // Apply 'filter' to the table data to only keep the
+        // rows where the 'datetime' value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    // Rebuild the table using the filtered data
+    // @NOTE If no date was entered, then filterData will
+    // just be the original tableData.
+    buildTable(filteredDate);
+};
+
+d3.selectAll("#filter-btn").on("click", handleClick);
